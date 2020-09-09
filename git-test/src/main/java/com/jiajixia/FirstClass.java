bb984a1fd40ddbcb6fb818355f8e011affc3d5f1
jiajixia <58507163+jiajixia@users.noreply.github.com>
2020-09-09 18:07:16 +0800

    V1.0

---
+++ git-test/src/main/java/com/jiajixia/FirstClass.java
@@ -0,0 +1,15 @@
+package com.jiajixia;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+    }
+}
